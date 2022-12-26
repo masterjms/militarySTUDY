4893b64cb2e569864cad9ed7351bdc4049c16bbf
Jang Min Sung <88541670+masterjms@users.noreply.github.com>
2022-12-26 20:20:18 +0900

    최빈값 코드 분석

--- basic js/최빈값.js
+++ basic js/최빈값.js
@@ -1,1 +1,30 @@
+function solution(arr) {
+    var answer = [1, 1];
+    let v = 0;
+    arr.sort();
 
+    for(let i=0; i<arr.length; i++){ //arr의 길이만큼 i값 증가
+        if(arr[i] == arr[i+1]){
+            v++;
+            if(arr[i+1] !== arr[i+2]){
+                v++;
+                if(answer[1] < v){
+                    answer = [];
+                    answer.push(arr[i], v);
+                } else if (answer[1] == v){
+                    answer.push(arr[i], v);
+                }
+                v = 0;
+            }
+        }
+    }
+
+    if(answer.length == 2){
+        return answer[0];
+    } else if(answer.length > 2){
+        return -1;
+    } else {
+        return arr[0];
+    }
+
+}
